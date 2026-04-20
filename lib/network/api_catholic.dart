ba536978f7bb5d9c8853a24881d1e5162c75a1f4
Ztivy <ztivy1174@gmail.com>
2026-04-20 09:46:06 -0600

    integracion del futuregroup y de una lista general donde se recuperan todos los datos de las otras listas

--- lib/network/api_catholic.dart
+++ lib/network/api_catholic.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:async/async.dart';
 import 'package:ztv/models/series_model.dart';
 import 'package:ztv/utils/strings_app.dart';
 class ApiCatholic{
@@ -8,5 +9,17 @@ class ApiCatholic{
     final response = await dio.get('${StringsApp.url_base}/json/categories/$idSerie.json');
     final res = response.data('series') as List;
     return res.map((series)=>SeriesDao.fromMap(series)).toList();
+  }
+
+  Future <List<List<SeriesDao>>> getAllCategories() async{
+    final FutureGroup<List<SeriesDao>> futureGroup = FutureGroup();
+    futureGroup.add(getSeries(2));
+    futureGroup.add(getSeries(3));
+    futureGroup.add(getSeries(4));
+    futureGroup.add(getSeries(5));
+    futureGroup.close();
+
+    final List<List<SeriesDao>> results = await futureGroup.future;
+    return results;
   }
 }
